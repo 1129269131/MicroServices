7f6ee720bc27f8a548d0071ef129479a3d93cd65
koala <1129269131@qq.com>
2021-09-28 22:44:22 +0800

    /eureka-clinet复习

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MicroServices
  微服务框架源码解析
+ 
+ 复习说明：
+	一、eureka
+		1.1、eureka-02-provider-8081
+		1.2、eureka-01-server-8000
--- eureka2/eureka-03-consumer-8080/src/main/java/com/abc/consumer/bean/Depart.java
+++
@@ -1,9 +0,0 @@
-package com.abc.consumer.bean;
-
-import lombok.Data;
-
-@Data
-public class Depart {
-    private Integer id;
-    private String name;
-}
